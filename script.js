25fd55ebfbd6c5f9fe30bf0736cf0910d574a35f
Kennedy <kennedyjusticespencer@gmail.com>
2022-06-29 11:13:10 -0500

    delete functionality added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let numbers = document.querySelectorAll('.number')
+const numbers = document.querySelectorAll('.number')
 const screen = document.querySelector('#screen')
+const clear = document.querySelector('#clear')
+const del = document.querySelector('#delete')
 
 numbers.forEach((button) => {
     button.addEventListener('click', (e) => {
@@ -15,3 +17,11 @@ function display(input) {
     screen.textContent = screen.textContent.concat(input);
     }
 }
+
+clear.addEventListener('click', () => {
+    screen.textContent = ''
+})
+
+del.addEventListener('click', () => {
+    screen.textContent = screen.textContent.slice(0, -1)
+})
